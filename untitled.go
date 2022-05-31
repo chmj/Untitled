84cf24bff50179dae5fc732d2d73cd955ecc8290
Charles Majola <chmj@rootcore.com>
2022-05-31 14:59:57 +0400

    Nested Maps and Pointers

--- untitled.go
+++ untitled.go
@@ -1,62 +1,32 @@
 package main
 
-import (
-	"fmt"
-)
-
-/*
-Struct Value Literals and Struct Value Manipulations
-
-In Go, the form T{...}, where T must be a type literal or a type name, is called a composite literal and is used as the
-value literals of some kinds of types, including struct types and the container types introduced later.
-
-Note, a type literal T{...} is a typed value, its type is T.
-Given a struct type S whose underlying type is struct{x int; y bool}, the zero value of S can be represented by the
-following two variants of struct composite literal forms:
-
-    S{0, false}. In this variant, no field names are present but all field values must be present by the field
-declaration orders.
-    S{x: 0, y: false}, S{y: false, x: 0}, S{x: 0}, S{y: false} and S{}. In this variant, each field item is optional
-and the order of the field items is not important. The values of the absent fields will be set as the zero values of
-their respective types. But if a field item is present, it must be presented with the FieldName: FieldValue form. The
-order of the field items in this form doesn't matter. The form S{} is the most used zero value representation of type S.
-
-If S is a struct type imported from another package, it is recommended to use the second form, to maintain
-compatibility. Consider the case where the maintainer of the package adds a new field for type S, this will make the
-use of first form invalid.
-
-Surely, we can also use the struct composite literals to represent non-zero struct value.
-
-For a value v of type S, we can use v.x and v.y, which are called selectors (or selector expressions), to represent the
-field values of v. v is called the receiver of the selectors. Later, we call the dot . in a selector as the property
-selection operator.
-An example:
-*/
-
-type Book struct {
-	title, author string
-	pages         int
-}
+import "fmt"
 
 func main() {
-	book := Book{"Go 101", "Tapir", 256}
-	fmt.Println(book) // {Go 101 Tapir 256}
+	// Nested Map[struct]->Map[string]->int
+	languages := map[struct{ dynamic, strong bool }]map[string]int{
+		{true, false}:  {"JavaScript": 1995},
+		{false, true}:  {"Go": 2009},
+		{false, false}: {"C": 1972},
+	}
+	// The key type and element type of this map
+	// are both pointer types. Some weird, just
+	// for education purpose.
+	m0 := map[*struct{ dynamic, strong bool }]*map[string]int{}
+	for category, langInfo := range languages {
+		m0[&category] = &langInfo
+		// This following line has no effects on languages.
+		category.dynamic, category.strong = true, true
+	}
+	for category, langInfo := range languages {
+		fmt.Println(category, langInfo)
+	}
 
-	// Create a book value with another form.
-	// All of the three fields are specified.
-	book = Book{author: "Tapir", pages: 256, title: "Go 101"}
-
-	// None of the fields are specified. The title and
-	// author fields are both "", pages field is 0.
-	book = Book{}
-
-	// Only specify the author field. The title field
-	// is "" and the pages field is 0.
-	book = Book{author: "Tapir"}
-
-	// Initialize a struct value by using selectors.
-	var book2 Book // <=> book2 := Book{}
-	book2.author = "Tapir Liu"
-	book2.pages = 300
-	fmt.Println(book2.pages) // 300
+	m1 := map[struct{ dynamic, strong bool }]map[string]int{}
+	for category, langInfo := range m0 {
+		m1[*category] = *langInfo
+	}
+	// m0 and m1 both contain only one entry.
+	fmt.Println(len(m0), len(m1)) // 1 1
+	fmt.Println(m1)               // map[{true true}:map[C:1972]]
 }
