acf001e3013ee65fbf26b54d7cf10e8e79a9f1a3
Charles Majola <chmj@rootcore.com>
2022-05-31 15:20:19 +0400

    Variadic Function Calls

--- untitled.go
+++ untitled.go
@@ -2,31 +2,35 @@ package main
 
 import "fmt"
 
-func main() {
-	// Nested Map[struct]->Map[string]->int
-	languages := map[struct{ dynamic, strong bool }]map[string]int{
-		{true, false}:  {"JavaScript": 1995},
-		{false, true}:  {"Go": 2009},
-		{false, false}: {"C": 1972},
-	}
-	// The key type and element type of this map
-	// are both pointer types. Some weird, just
-	// for education purpose.
-	m0 := map[*struct{ dynamic, strong bool }]*map[string]int{}
-	for category, langInfo := range languages {
-		m0[&category] = &langInfo
-		// This following line has no effects on languages.
-		category.dynamic, category.strong = true, true
-	}
-	for category, langInfo := range languages {
-		fmt.Println(category, langInfo)
-	}
+/*
+Variadic function calls
+There are two manners to pass arguments to a variadic parameter of type []T:
 
-	m1 := map[struct{ dynamic, strong bool }]map[string]int{}
-	for category, langInfo := range m0 {
-		m1[*category] = *langInfo
+    pass a slice value as the only argument. The slice must be assignable to values of type []T, and the slice must be
+	followed by three dots .... The passed slice is called as a variadic argument.
+    pass zero or more arguments which are assignable to values of type T. These arguments will be copied (or converted)
+	as the elements of a new allocated slice value of type []T, then the new allocated slice will be passed to the
+	variadic parameter.
+
+Note, the two manners can't be mixed in the same variadic function call.
+An example program which uses some variadic function calls:
+*/
+
+func Concat(sep string, tokens ...string) (r string) {
+	for i, t := range tokens {
+		if i != 0 {
+			r += sep
+		}
+		r += t
 	}
-	// m0 and m1 both contain only one entry.
-	fmt.Println(len(m0), len(m1)) // 1 1
-	fmt.Println(m1)               // map[{true true}:map[C:1972]]
+	return
+}
+
+func main() {
+	tokens := []string{"Go", "C", "Rust"}
+	// manner 1
+	langsA := Concat(",", tokens...)
+	// manner 2
+	langsB := Concat(",", "Go", "C", "Rust")
+	fmt.Println(langsA == langsB) // true
 }
