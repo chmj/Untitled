c53103b2c0401211b50709f0642eb5a780794372
Charles Majola <chmj@rootcore.com>
2022-05-31 15:25:43 +0400

    Basic Channel Use

--- untitled.go
+++ untitled.go
@@ -1,36 +1,36 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"time"
+)
 
 /*
-Variadic function calls
-There are two manners to pass arguments to a variadic parameter of type []T:
+Some Channel Use Examples
 
-    pass a slice value as the only argument. The slice must be assignable to values of type []T, and the slice must be
-	followed by three dots .... The passed slice is called as a variadic argument.
-    pass zero or more arguments which are assignable to values of type T. These arguments will be copied (or converted)
-	as the elements of a new allocated slice value of type []T, then the new allocated slice will be passed to the
-	variadic parameter.
-
-Note, the two manners can't be mixed in the same variadic function call.
-An example program which uses some variadic function calls:
+Now that you've read the above section, let's view some examples which use channels to enhance your understanding.
+A simple request/response example. The two goroutines in this example talk to each other through an unbuffered channel.
 */
 
-func Concat(sep string, tokens ...string) (r string) {
-	for i, t := range tokens {
-		if i != 0 {
-			r += sep
-		}
-		r += t
-	}
-	return
-}
-
 func main() {
-	tokens := []string{"Go", "C", "Rust"}
-	// manner 1
-	langsA := Concat(",", tokens...)
-	// manner 2
-	langsB := Concat(",", "Go", "C", "Rust")
-	fmt.Println(langsA == langsB) // true
+	c := make(chan int) // an unbuffered channel
+	go func(ch chan<- int, x int) {
+		time.Sleep(time.Second)
+		// <-ch    // fails to compile
+		// Send the value and block until the result is received.
+		ch <- x * x // 9 is sent
+	}(c, 3)
+	done := make(chan struct{})
+	go func(ch <-chan int) {
+		// Block until 9 is received.
+		n := <-ch
+		fmt.Println(n) // 9
+		// ch <- 123   // fails to compile
+		time.Sleep(time.Second)
+		done <- struct{}{}
+	}(c)
+	// Block here until a value is received by
+	// the channel "done".
+	<-done
+	fmt.Println("bye")
 }
